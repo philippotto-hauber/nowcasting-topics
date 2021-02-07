7ff0bd3a792d13aff0ebb9114a9c00ce6e5dac21
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-02-07 10:58:07 +0100

    adjust T0 for lags of daily factor
    - previous code placed -W_qd_c directly after I_Nr corresponding to f_t
    - also move W_qd_c and W_qd_p to params in function

--- model/f_state_space_params.m
+++ model/f_state_space_params.m
@@ -1,4 +1,4 @@
-function [Z, H, T, R, Q] = f_state_space_params(params, W_qd_c, W_qd_p)
+function [Z, H, T, R, Q] = f_state_space_params(params)
 
 % back out dimensions of state space system
 Nd = size(params.lam_d, 1);
@@ -24,21 +24,38 @@ T0(Nr*Np_eff+1:Ns,1:Nr) = repmat(-eye(Nr), (Ns - Nr*Np_eff)/Nr, 1);
 iT0 = T0 \ eye(Ns); 
 
 T = NaN(Ns, Ns, Nt);
-% for t = 1:Nt
-%     Ttmp = [params.Phi zeros(Nr, Ns-size(params.Phi, 2));
-%             eye(Nr * (Np_eff-1)) zeros(Nr * (Np_eff-1), Ns - Nr * (Np_eff-1))];
-%     if Nw > 0 && Nd > 0 % if there are weekly series and its not the highest frequency. Otherwise its dynamics are governed by phi_f!
-%         Ttmp = [Ttmp; zeros(Nr, size(Ttmp, 1)) params.Xi_w(t) * eye(Nr) zeros(Nr, Ns - (size(Ttmp, 1) + Nr))];
-%     end
-%     if Nm > 0 && (Nw > 0 || Nd > 0) 
-%         Ttmp = [Ttmp; zeros(Nr, size(Ttmp, 1)) params.Xi_m(t) * eye(Nr) zeros(Nr, Ns - (size(Ttmp, 1) + Nr))];
-%     end
-%     if Nq > 0 && (Nm > 0 || Nw > 0 || Nd > 0)
-%         Ttmp = [Ttmp; zeros(Nr, size(Ttmp, 1)) params.Xi_q(t) * eye(Nr) zeros(Nr, Ns - (size(Ttmp, 1) + Nr))];
-%     end
-% 
-%     T(:, :, t) = iT0 * Ttmp;
-% end
+for t = 1:Nt
+    T0 = eye(Ns);
+    if Nq_flow > 0; T0(Nr*Np_eff+1:(Np_eff+2)*Nr, 1:Nr) = [-params.W_qd_c(t) * eye(Nr); -params.W_qd_p(t) * eye(Nr)];end
+    if Nq_stock > 0; T0((Np_eff+2)*Nr+1:(Np_eff+3)*Nr, 1:Nr) = -eye(Nr);end
+    iT0 = T0 \ eye(Ns);
+        
+    Ttmp = [params.Phi zeros(Nr, Ns-size(params.Phi, 2));
+            eye(Nr * (Np_eff-1)) zeros(Nr * (Np_eff-1), Ns - Nr * (Np_eff-1))];
+    if Nw > 0 && Nd > 0 % if there are weekly series and its not the highest frequency. Otherwise its dynamics are governed by phi_f!
+        Ttmp = [Ttmp; zeros(Nr, size(Ttmp, 1)) params.Xi_w(t) * eye(Nr) zeros(Nr, Ns - (size(Ttmp, 1) + Nr))];
+    end
+    if Nm > 0 && (Nw > 0 || Nd > 0) 
+        Ttmp = [Ttmp; zeros(Nr, size(Ttmp, 1)) params.Xi_m(t) * eye(Nr) zeros(Nr, Ns - (size(Ttmp, 1) + Nr))];
+    end
+    if Nq_flow > 0 && (Nm > 0 || Nw > 0 || Nd > 0)
+        if params.Xi_q(t) == 0
+            Ttmp = [Ttmp; zeros(2*Nr, size(Ttmp, 1)) zeros(2*Nr, Nr) [eye(Nr); zeros(Nr)] zeros(2*Nr, Ns - (size(Ttmp, 1) + 2*Nr))];
+        else
+            Ttmp = [Ttmp; zeros(2*Nr, size(Ttmp, 1)) eye(2*Nr) zeros(2*Nr, Ns - (size(Ttmp, 1) + 2*Nr))];
+        end
+        %Ttmp = [Ttmp; zeros(Nr, size(Ttmp, 1)) params.Xi_q(t) * eye(Nr) zeros(Nr, Ns - (size(Ttmp, 1) + Nr))];
+    end
+    if Nq_stock > 0 && (Nm > 0 || Nw > 0 || Nd > 0)
+        if params.Xi_q(t) == 0
+            Ttmp = [Ttmp; zeros(Nr, size(Ttmp, 1)) zeros(Nr) zeros(Nr, Ns - (size(Ttmp, 1) + Nr))];
+        else
+            Ttmp = [Ttmp; zeros(Nr, size(Ttmp, 1)) eye(Nr) zeros(Nr, Ns - (size(Ttmp, 1) + Nr))];
+        end
+        %Ttmp = [Ttmp; zeros(Nr, size(Ttmp, 1)) params.Xi_q(t) * eye(Nr) zeros(Nr, Ns - (size(Ttmp, 1) + Nr))];
+    end
+    T(:, :, t) = iT0 * Ttmp;
+end
 
 % Q & R 
 Q = zeros(Ns);

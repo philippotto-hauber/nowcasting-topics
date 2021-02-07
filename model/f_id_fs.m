cde1dfddbb507f75edfe848f85332778917289f0
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-02-07 11:45:55 +0100

    fix bug, adjust args

--- model/f_id_fs.m
+++ model/f_id_fs.m
@@ -1,4 +1,4 @@
-function [id_f, id_f_lags, id_f_d, id_f_w, id_f_m, id_f_q] = f_id_fs(Nr, Np_eff, Nd, Nw, Nm, Nq)
+function [id_f, id_f_lags, id_f_d, id_f_w, id_f_m, id_f_q_flow, id_f_q_stock] = f_id_fs(Nr, Np_eff, Nd, Nw, Nm, Nq_flow, Nq_stock)
 
 id_f = 1:Nr; % note that id_f will be identical to id_f_d, id_f_w, ... always refering to the factor of the highest frequency!
 id_f_lags = Nr+1:Nr * Np_eff;
@@ -31,12 +31,19 @@ else
    id_f_m = [];
 end
 
-if Nq > 0
-    id_f_q = ind_s+1 : ind_s+Nr;
+if Nq_flow > 0
+    id_f_q_flow = ind_s+1 : ind_s+Nr;
+    ind_s = ind_s + 2*Nr; % zeros due to f^QP_t
 else
-    id_f_q = []; 
+    id_f_q_flow = []; 
 end
 
+
+if Nq_stock > 0
+    id_f_q_stock = ind_s+1 : ind_s+Nr;
+else
+    id_f_q_stock = []; 
+end
  
 
 

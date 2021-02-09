dbb40f7df698073e9f4df5e0cf9a39762f518a85
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-02-09 21:59:08 +0100

    rm ind_o from f_ols
    - can be directly calculated from y
    - ind_o and f_ind_o is only ever needed with artificial data!
    - this means that the function no longer requires Xi as an input

--- model/f_start_vals.m
+++ model/f_start_vals.m
@@ -11,7 +11,7 @@ f = f_PCA(y_d, Nr);
 f  = f ./ std(f, [], 2);
 
 % lam_d, sig2_d
-[params.lam_d, params.sig2_d] = f_ols(y_d, f, zeros(size(f, 2), 1));
+[params.lam_d, params.sig2_d] = f_ols(y_d, f);
 
 %-----------------------------------%
 %- lam_w, sig2_w
@@ -21,7 +21,7 @@ f  = f ./ std(f, [], 2);
 [f_w, ~] = f_cum_f(f, aux.Xi_wd, zeros(size(aux.Xi_wd)), zeros(size(aux.Xi_wd)));
 
 % lam_w and sig2_w
-[params.lam_w, params.sig2_w] = f_ols(y_w, f_w, aux.Xi_wd);
+[params.lam_w, params.sig2_w] = f_ols(y_w, f_w);
 
 %-----------------------------------%
 %- lam_m, sig2_m
@@ -31,10 +31,10 @@ f  = f ./ std(f, [], 2);
 [f_m, f_m_c] = f_cum_f(f, aux.Xi_md, aux.W_md_c, aux.W_md_p);
 
 % lam_m_stock and sig2_m_stock
-[params.lam_m_stock, params.sig2_m_stock] = f_ols(y_m(~aux.ind_m_flow, :), f_m, aux.Xi_md);
+[params.lam_m_stock, params.sig2_m_stock] = f_ols(y_m(~aux.ind_m_flow, :), f_m);
 
 % lam_m_stock and sig2_m_stock
-[params.lam_m_flow, params.sig2_m_flow] = f_ols(y_m(aux.ind_m_flow, :), f_m_c, aux.Xi_md);
+[params.lam_m_flow, params.sig2_m_flow] = f_ols(y_m(aux.ind_m_flow, :), f_m_c);
 
 %-----------------------------------%
 %- lam_q, sig2_q
@@ -44,10 +44,10 @@ f  = f ./ std(f, [], 2);
 [f_q, f_q_c] = f_cum_f(f, aux.Xi_qd, aux.W_qd_c, aux.W_qd_p);
 
 % lam_q_stock and sig2_q_stock
-[params.lam_q_stock, params.sig2_q_stock] = f_ols(y_q(~aux.ind_q_flow, :), f_q, aux.Xi_qd);
+[params.lam_q_stock, params.sig2_q_stock] = f_ols(y_q(~aux.ind_q_flow, :), f_q);
 
 % lam_q_flow and sig2_q_flow
-[params.lam_q_flow, params.sig2_q_flow] = f_ols(y_q(aux.ind_q_flow, :), f_q_c, aux.Xi_qd);
+[params.lam_q_flow, params.sig2_q_flow] = f_ols(y_q(aux.ind_q_flow, :), f_q_c);
 end
 
 function F_hat = f_PCA(Y,R)
@@ -77,15 +77,8 @@ end
 A_reverse = aux;
 end
 
-function [lam, sig2] = f_ols(y, f, Xi)
-Nt = size(f, 2); 
-tmp = 1:Nt;
-if all(Xi == 0)
-    ind_o = repelem(true, Nt);
-else
-    ind_o = tmp(Xi == 0) - 1;
-    ind_o = ind_o(ind_o > 0);
-end
+function [lam, sig2] = f_ols(y, f)
+ind_o = find(~any(isnan(y), 1)); 
 x_estim = f(:, ind_o)';
 y_estim = y(:, ind_o)';
 
